62a0e0331f1abb2bd7aab7d3cb2339ff20dc82e2
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-01-26 15:27:49 +0200

    Fix parsing of video sizes and endpoints

--- streaming/helpers.py
+++ streaming/helpers.py
@@ -1,13 +1,27 @@
+class Endpoint:
+    def __init__(self, endp_id, dc_latency, caches_connections):
+        self.id = endp_id
+        self.latency = dc_latency  # latency for requesting video directly from datacenter
+        self.caches_connections = caches_connections
+
+    def __repr__(self):
+        return f"E{self.id}: {self.latency}, {self.caches_connections}"
+
+
 def parse(input_f):
     with open(input_f, "r") as f:
         n_vid, n_endp, n_req, n_ser, cap_ser = map(int, f.readline().split())
         vid_sizes = []
         endps = []
         reqs = []
-        for _ in range(n_vid):
-            vid_sizes.append(list(map(int, f.readline().split())))
-        for _ in range(n_endp):
-            endps.append(list(map(int, f.readline().split())))
+        vid_sizes = list(map(int, f.readline().split()))
+        for endpoint_id in range(n_endp):
+            dc_latency, n_caches = list(map(int, f.readline().split()))
+            caches_connections = {}
+            for cache in range(n_caches):
+                cs_id, cs_latency = list(map(int, f.readline().split()))
+                caches_connections[cs_id] = cs_latency
+            endps.append(Endpoint(endpoint_id, dc_latency, caches_connections))
         for _ in range(n_req):
             reqs.append(list(map(int, f.readline().split())))
     return n_vid, n_endp, n_req, n_ser, cap_ser, vid_sizes, endps, reqs
