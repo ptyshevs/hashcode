0aca0a26b7dba3f5b11e95fa8a912982e49d1e3c
Andrii STADNIK <astadnik@student.unit.ua>
2019-02-14 20:10:41 +0200

    Checker works for the example

--- data_centers/submit.py
+++ data_centers/submit.py
@@ -3,10 +3,10 @@ import argparse
 import sys
 
 
-def score(R, P):
+def score(R, pools):
     min_cap = 1e17
     for row in range(R):
-        for pool in range(P):
+        for pool in pools:
             capacity = 0
             for server, r in pool:
                 if r != row:
@@ -24,29 +24,23 @@ def evaluate(args):
     pools = [[] for _ in range(P)]
     with open(args.filename, "r") as f:
         for i in range(M):
-            print("HM1")
             line = f.readline().split()
-            if line == 'x':
+            if line[0] == 'x' and len(line) == 1:
                 continue
 
-            server = servers[i - 1]
+            server = servers[i]
             a_r, a_s, a_p = list(map(int, line))
-            print(a_p + server[0], P)
             if a_r < 0 or a_r >= R or\
-                    a_s < 0 or a_s + server[0] >= S or\
+                    a_s < 0 or a_s + server[0] - 1 >= S or\
                     a_p < 0 or a_p >= P:
-                __import__('ipdb').set_trace()
                 raise RuntimeError("Wrong position at line {}".format(i))
 
             for j in range(server[0]):
                 if rows[a_r][a_s + j]:
                     raise RuntimeError("Server overlaps with another "
                                        "on line {}".format(i))
-                print("HM1.3")
                 rows[a_r][a_s + j] = True
-                print("HM1.6")
             pools[a_p].append([server, a_r])
-            print("HM2")
 
         try:
             f.readline()
@@ -54,7 +48,7 @@ def evaluate(args):
         except:
             pass
     print("HEHEY IT DIDN'T BREAK")
-    score(R, P)
+    score(R, pools)
 
 
 def pargs():
