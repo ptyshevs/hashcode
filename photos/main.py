372539f89ece37be9c4a614afe015688df4d5d33
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-02-28 20:33:37 +0200

    Add real submit

--- photos/main.py
+++ photos/main.py
@@ -1,13 +1,5 @@
 from parser import *
 
-def submit(slideshow, path="test.out"):
-    with open(path, "w+") as f:
-        print(len(slideshow), file=f)
-        for slide in slideshow:
-            if type(slide) == Photo:
-                print(slide.id, file=f)
-            else:
-                print(f"{slide[0].id} {slide[1].id}", file=f)
 
 if __name__ == '__main__':
     photos = get_photos()
