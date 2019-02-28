776eae85bc00ea96638a4d7baa55bd33498c5acd
Anastasii KONDAUROVA <akondaur@e1r5p5.unit.ua>
2019-02-28 22:34:14 +0200

    1

--- photos/set_photos.py
+++ photos/set_photos.py
@@ -15,12 +15,13 @@ def get_slides(path):
     photos = parser.get_photos(path)
     slides = []
     all_posibilities = [[Slide(x1, x2), len(x1.tags & x2.tags)] for x1 in photos for x2 in photos if x1.type == 'V' and x2.type == 'V' and x1 != x2]
+    a = -1
     for x1 in photos:
         x1_containers = [x for x in all_posibilities if x[0].ids[0] == x1.id]
         if len(x1_containers) > 0:
             a = max(x1_containers, key=lambda x: x[1])[0]
             slides.append(a)
-        all_posibilities = [x for x in all_posibilities if x[0].ids[0] not in a.ids and x[0].ids[1] not in a.ids]
+            all_posibilities = [x for x in all_posibilities if x[0].ids[0] not in a.ids and x[0].ids[1] not in a.ids]
     for x1 in photos:
         if x1.type == 'H':
             slides.append(x1)
